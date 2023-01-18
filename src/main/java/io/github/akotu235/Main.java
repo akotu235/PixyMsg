0891a29e4a8e094ebe72fa721cc9e910cddfa385
akotu235 <akotu@protonmail.com>
2023-01-19 00:23:25 +0100

    created GUI

--- src/main/java/io/github/akotu235/Main.java
+++ src/main/java/io/github/akotu235/Main.java
@@ -1,36 +1,10 @@
 package io.github.akotu235;
 
-import io.github.akotu235.pixymsg.PixelModifier;
-import io.github.akotu235.pixymsg.PixelReader;
-
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
+import io.github.akotu235.view.PixyMsgGUI;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Input file: ");
-        String inputFilePath = scanner.nextLine();
-
-        System.out.print("Message: ");
-        String msg = scanner.nextLine();
-
-        String outputFileName = "image";
-        outputFileName += ".bmp";
-
-        //Creating an image with a hidden message.
-        PixelModifier pixelModifier = new PixelModifier(ImageIO.read(new File(inputFilePath)), msg);
-
-        //Saving the picture.
-        File outputfile = new File(outputFileName);
-        ImageIO.write(pixelModifier.getImage(), "bmp", outputfile);
-
-        //Reading a hidden message.
-        PixelReader pixelReader = new PixelReader(ImageIO.read(new File(outputFileName)));
-        System.out.println("Hidden message:");
-        System.out.println(pixelReader.getMsg());
+    public static void main(String[] args) {
+        PixyMsgGUI window = new PixyMsgGUI();
+        window.setVisible(true);
     }
 }
---
+++ src/main/java/io/github/akotu235/view/PixyMsgGUI.java
@@ -0,0 +1,120 @@
+package io.github.akotu235.view;
+
+import io.github.akotu235.pixymsg.PixelModifier;
+import io.github.akotu235.pixymsg.PixelReader;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+public class PixyMsgGUI extends JFrame {
+    private JPanel mainPanel;
+    private JPanel addMsgPanel;
+    private JPanel readMsgPanel;
+    private JButton imageLoadButton;
+    private JTextField msgLoadTextField;
+    private JButton msgLoadButton;
+    private JLabel label;
+    private JTextArea msg;
+
+    private PixelReader pixelReader;
+    private PixelModifier pixelModifier;
+    private String filePath;
+    private String fileName;
+    public PixyMsgGUI() {
+        setTitle("PixyMsg");
+        setSize(400, 120);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        mainPanel = new JPanel();
+        addMsgPanel = new JPanel();
+        readMsgPanel = new JPanel();
+        label = new JLabel();
+        imageLoadButton = new JButton("Load image");
+        msgLoadTextField = new JTextField(20);
+        msgLoadButton = new JButton("Hide text in image");
+        msg = new JTextArea();
+
+        imageLoadButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setCurrentDirectory(new File(System.getProperty("user.home") + "/Desktop"));
+                int returnValue = fileChooser.showOpenDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    filePath = fileChooser.getSelectedFile().getAbsolutePath();
+                    String fileNameWithExtension  = fileChooser.getSelectedFile().getName();
+
+                    int index = fileNameWithExtension.lastIndexOf(".");
+                    fileName = fileNameWithExtension.substring(0, index);
+
+                    try {
+                        pixelReader = new PixelReader(ImageIO.read(new File(filePath)));
+                    } catch (IOException ex) {
+                        readMsgPanel.setVisible(false);
+                        addMsgPanel.setVisible(false);
+                        throw new RuntimeException(ex);
+                    }
+                    if (pixelReader.hasMessage()) {
+                        label.setText("Hidden message in "+ fileName);
+                        msg.setText(pixelReader.getMsg());
+                        readMsgPanel.setVisible(true);
+                        addMsgPanel.setVisible(false);
+                    } else {
+                        label.setText("No message");
+                        addMsgPanel.setVisible(true);
+                        readMsgPanel.setVisible(false);
+                    }
+                }
+            }
+        });
+
+        msgLoadButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                pixelModifier = new PixelModifier(pixelReader.getImage(), msgLoadTextField.getText());
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setDialogTitle("Select directory to save the file");
+                fileChooser.setApproveButtonText("Save");
+                fileChooser.setCurrentDirectory(new File(System.getProperty("user.home") + "/Desktop"));
+                fileChooser.setFileSelectionMode(JFileChooser.SAVE_DIALOG);
+                int returnValue = fileChooser.showOpenDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    String directoryPath = selectedFile.getAbsolutePath();
+                    File outputFile = new File(directoryPath + "\\" + fileName + "[PixyMsg].bmp");
+                    try {
+                        ImageIO.write(pixelModifier.getImage(), "bmp", outputFile);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    label.setText(directoryPath + "\\" + fileName + "[PixyMsg].bmp created");
+                    label.setVisible(true);
+                    addMsgPanel.setVisible(false);
+                    readMsgPanel.setVisible(false);
+                }
+            }
+        });
+
+        mainPanel.setLayout(new BorderLayout());
+        msg.setOpaque(false);
+        msg.setEditable(false);
+
+        mainPanel.add(imageLoadButton, BorderLayout.NORTH);
+        mainPanel.add(label, BorderLayout.SOUTH);
+        mainPanel.add(addMsgPanel, BorderLayout.EAST);
+        mainPanel.add(readMsgPanel, BorderLayout.CENTER);
+        addMsgPanel.add(msgLoadTextField);
+        addMsgPanel.add(msgLoadButton);
+        readMsgPanel.add(msg);
+
+        addMsgPanel.setVisible(false);
+        readMsgPanel.setVisible(false);
+
+        add(mainPanel);
+    }
+}
